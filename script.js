3b67c2be878ddfb4c434142157cc871c08467d1e
Sarvesh Kumar <143215832+Sarveshk01@users.noreply.github.com>
2023-10-22 17:15:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,64 @@
-var click_down=document.querySelector(".top span");
-var card=document.querySelector(".card");
-var up=document.querySelector(".top span i");
-click_down.addEventListener('click',function(){
-    card.classList.toggle('heighten');
-    if(up.classList.contains("fa-angle-down")){  
-     up.classList.remove('fa-angle-down');
-     up.classList.add('fa-angle-up');
-    } 
-    else{
-            up.classList.add('fa-angle-down');
-     up.classList.remove('fa-angle-up'); 
-    }
-    
-}); 
- 
+let toggleBtn = document.getElementById('toggle-btn');
+let body = document.body;
+let darkMode = localStorage.getItem('dark-mode');
 
-var progress_bar = document.querySelectorAll(".progress-bar li");
-function checked(id){ 
-       var checked = document.getElementById("check"+id);
-       checked.classList.toggle("list_checked");
-          var striked = document.getElementById("strike"+id);
-       striked.classList.toggle("strike_none");
-        var list_childs = document.getElementById(id);
-       list_childs.classList.toggle("list_active");
-       progress(); 
-}  
-function progress(){
-      var checked_progress=document.querySelectorAll(".list li.list_active");
-      var length = checked_progress.length;
-       progress_bar.forEach(function(index){
-           index.classList.remove("active");
-       });
-   
-      for(i=0;i<length;i++){
-          progress_bar[i].classList.add("active");
-      } 
-      
-      var checked_tick = document.querySelector("#checked_toggle");
-      if(length >4){
-          
-          checked_tick.classList.remove("d-none");
-      }else{
-          checked_tick.classList.add("d-none");
-      }
-       
+const enableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
+}
+
+if(darkMode === 'enabled'){
+   enableDarkMode();
+}
+
+toggleBtn.onclick = (e) =>{
+   darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === 'disabled'){
+      enableDarkMode();
+   }else{
+      disableDarkMode();
+   }
+}
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   search.classList.remove('active');
+}
+
+let search = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   search.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+let sideBar = document.querySelector('.side-bar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   sideBar.classList.toggle('active');
+   body.classList.toggle('active');
+}
+
+document.querySelector('#close-btn').onclick = () =>{
+   sideBar.classList.remove('active');
+   body.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   profile.classList.remove('active');
+   search.classList.remove('active');
+
+   if(window.innerWidth < 1200){
+      sideBar.classList.remove('active');
+      body.classList.remove('active');
+   }
 }
